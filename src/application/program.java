d76483d5bfada681315c76e1e3bbe562078e70a8
Gabriel Rocha <afiliadogrocha@gmail.com>
2023-04-15 03:26:27 -0300

    atualizacao

--- src/application/program.java
+++ src/application/program.java
@@ -1,13 +1,32 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPosition;
+import chess.chessPiece;
 
 public class program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while (true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChesPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChesPosition(sc);
+			
+			
+			chessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 	
 }
